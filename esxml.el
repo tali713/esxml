7a3eeaab02f31a8cb4ded0779f92f0515feb1339
Evan Izaksonas-Smith <izak0002@umn.edu>
2012-09-30 18:00:56 -0500

    minor fix * esxml.el: oops, over zealous editting.  esxml-textarea

--- esxml.el
+++ esxml.el
@@ -162,8 +162,8 @@ VALUE is optional, if it's supplied whatever is supplied is used.
 (defun esxml-textarea (name &optional content)
   "Make an HTML TextArea control."
   `(textarea ((name . ,name))
-             ,@(or content nil))) ;; this may be incorrect, textareas
-                                  ;; /may/ require a body
+             ,@(or (list content) nil)))
+;; this may be incorrect, textareas /may/ require a body
 
 (defun esxml-listify (body &optional ordered-p)
   `(,(if ordered-p 'ol 'ul) ()
