5548ceba17deae0c3c6d0092672edc4de3c75ce3
Vasilij Schneidermann <mail@vasilij.de>
2017-11-29 09:07:43 +0100

    Bump version

--- esxml-query.el
+++ esxml-query.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vasilij Schneidermann <mail@vasilij.de>
 ;; Maintainer: Vasilij Schneidermann
-;; Version: 0.0.1
+;; Version: 0.1.0
 ;; Keywords: data, lisp
 ;; Package-Requires: ((cl-lib "0.1"))
 ;;
