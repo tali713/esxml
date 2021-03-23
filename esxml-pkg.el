65f11582120a5cef4953ff461ae0405d119220ff
Vasilij Schneidermann <mail@vasilij.de>
2021-03-23 11:55:41 +0100

    Depend on cl-lib, bump version

--- esxml-pkg.el
+++ esxml-pkg.el
@@ -1,1 +1,2 @@
-(define-package "esxml" "0.3.5" "Library for working with xml via esxml and sxml")
+(define-package "esxml" "0.3.6" "Library for working with xml via esxml and sxml"
+  '((db "0.0.1") (cl-lib "0.5")))
